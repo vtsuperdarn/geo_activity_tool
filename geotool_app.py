2c2c6b7344b635599bd5c12a95831198f70a3cc5
bharatreddy <bharatr@vt.edu>
2020-05-22 22:40:24 -0400

    updated streamlit

--- geotool_app.py
+++ geotool_app.py
@@ -55,7 +55,8 @@ def ss_onset_page():
     tmp_plchldr_spin.image("misc/brewing2.gif")    
     
         
-def geo_activity_page(local_data_store="data/sqlite3/"):
+def geo_activity_page(local_data_store="data/sqlite3/",\
+                      plot_style="classic"):
     """
     Geo activity tool page
     """
@@ -72,14 +73,20 @@ def geo_activity_page(local_data_store="data/sqlite3/"):
                             all_param_list,\
                             default=all_param_list\
                             )
-    sdate = st.sidebar.date_input(
+    start_day = st.sidebar.date_input(
                  "Start Date",
-                 datetime.date(2018, 1, 1)
+                 datetime.date(2018, 1, 2)
             )
-    edate = st.sidebar.date_input(
-                 "End Date",
-                 datetime.date(2018, 1, 1)
+    start_time = st.sidebar.time_input(
+                 "Start Time",
+                 datetime.time(0, 0)
             )
+    sdate = datetime.datetime.combine(start_day, 
+                              start_time)
+    ndays_hours = st.sidebar.slider("Number of hours to plot", 0, 23, 0)
+    ndays_plot = st.sidebar.slider("Number of days to plot", 0, 15, 1)
+    edate = sdate + datetime.timedelta(days=ndays_plot,\
+                                       hours=ndays_hours)
     # ss onset predictions
     tmp_plchldr_txt = st.empty()
     tmp_plchldr_spin = st.empty()
@@ -90,15 +97,36 @@ def geo_activity_page(local_data_store="data/sqlite3/"):
     for _npar,_par in enumerate(all_param_list):
         if _par not in params_selected:
             flags[_npar] = False
-    
+    geo_plot = st.empty()
+#     geo_date = st.markdown(\
+#                   sdate.strftime("%Y%m%d-%H%M") +\
+#                    " to " + edate.strftime("%Y%m%d-%H%M")\
+#                   )
     geo_fig = fetch_data_plotting(\
-                         sdate, edate, plot_style="classic",\
+                         sdate, edate, plot_style=plot_style,\
                          flags=flags,\
                          local_data_store=local_data_store)
-    tmp_plchldr_spin.pyplot(geo_fig)
-#     st.pyplot(geo_fig)
-    
-#     sdate_state = session_state.get(plot_start_date = sdate, plot_end_date = edate)
+    tmp_plchldr_spin.empty()
+    geo_plot.pyplot(geo_fig)
+#     if st.sidebar.button("Prev day"):
+#         state = session_state.get(\
+#                             plot_start_date=sdate,\
+#                             plot_end_date=edate\
+#                             )
+#         state.plot_start_date = state.plot_start_date - datetime.timedelta(days=1)
+#         state.plot_end_date = state.plot_end_date - datetime.timedelta(days=1)
+#         geo_date.markdown(\
+#                   state.plot_start_date.strftime("%Y%m%d-%H%M") +\
+#                    " - " + state.plot_end_date.strftime("%Y%m%d-%H%M")\
+#                   )
+#         geo_fig = fetch_data_plotting(\
+#                         state.plot_start_date,\
+#                         state.plot_end_date,\
+#                         plot_style=plot_style,\
+#                         flags=flags,\
+#                         local_data_store=local_data_store)
+#         tmp_plchldr_spin.empty()
+#         geo_plot.pyplot(geo_fig)
     
     
 if __name__ == "__main__":
